036c86100c5ffec09a485e4617a264d4b9af77ec
hakimpower83 <amirhakimosman@gmail.com>
2026-05-01 12:17:26 +0800

    app.py

--- app.py
+++ app.py
@@ -724,7 +724,7 @@ Annual report text:
                 st.markdown(
                     """
                     <div class="section-heading">
-                    Key Risk Discloaure Quality Summary
+                    Key Risk Disclosure Quality Summary
                     </div>
                     """,
                     unsafe_allow_html=True
